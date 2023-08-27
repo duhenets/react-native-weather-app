2c7855681b64fb7c5349e1ce11fbb9e00df275af
Oleksandr Duhenets <duhenets@gmail.com>
2023-08-27 20:57:26 +0300

    adjusted locales

--- App/locales/en.ts
+++ App/locales/en.ts
@@ -7,7 +7,7 @@ const en = {
         search: 'Search',
         back: 'Back',
         page_not_found: 'Oops! Page not found',
-        go_home: 'Go Home',
+        go_home: 'Go Home!',
         something_went_wrong: 'Oops! Something went wrong',
     },
 };
